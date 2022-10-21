8b87b36c4fe6cdfe79bc33d5456362a6e31d2f80
Renan de Moraes <68373784+mpraes@users.noreply.github.com>
2022-10-21 16:45:07 -0300

    Criando classe caixa de papelão

---
+++ caixa_papelao.py
@@ -0,0 +1,7 @@
+class CaixaPapelao:
+  def __init__(self, cor, altura, profundidade, largura):
+    self.cor = cor
+    self.altura = altura
+    self.profundidade = profundidade
+    self.largura = largura
+   
